e25ead59f43207ce7a22c20baa265a6efe660a5f
Vladimir Fibe <motiw@icloud.com>
2024-01-02 16:29:14 +0600

    add font and colors

---
+++ Itinerary/Resources/Theme.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class Theme {
+    static let mainFontName = "FugazOne-Regular"
+    static let accent: UIColor = .accent
+    static let background: UIColor = .background
+    static let tint: UIColor = .brandTint
+}
