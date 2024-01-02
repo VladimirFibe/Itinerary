78b5a8feaf4c62d771c9da51fc6c819e2d5c2a5b
Vladimir Fibe <motiw@icloud.com>
2024-01-02 12:21:57 +0600

    Added model and functions for Trips

---
+++ Itinerary/TripFunctions.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class TripFunctions {
+    static func create(_ trip: TripModel) {
+
+    }
+
+    static func read() {
+
+    }
+
+    static func update(_ trip: TripModel) {
+
+    }
+
+    static func delete(_ trip: TripModel) {
+
+    }
+}
